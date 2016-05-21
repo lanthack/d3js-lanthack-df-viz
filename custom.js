9bacf796cfad187de56749afd146c29f09681ce5
ankur-gupta <ankur@perfectlyrandom.org>
2016-05-21 15:43:15 -0700

    Still works

--- custom.js
+++ custom.js
@@ -3,6 +3,7 @@
 var reader = new FileReader();
 var fileSize;
 var fileName;
+var fileType;
 var dataset;
 
 function loadFile() {
@@ -12,29 +13,39 @@ function loadFile() {
     if (file) {
         fileName = file.name;
         fileSize = file.size;
+        fileType = file.type;
         reader.readAsText(file);
+        printFileDetails();
     }
 }
 
 function parseFile() {
     console.log("Parsing file ... ");
-    dataset = d3.csv.parse(reader.result)
+    dataset = d3.csv.parse(reader.result);
+    printFile(dataset);
 }
 
-function printFileDetails() {
+var printFileDetails = function() {
     console.log(fileName);
     console.log(fileSize);
     d3.select('body').append('p').text('File Name: ' + fileName);
     d3.select('body').append('p').text('File Size: ' + fileSize + 'bytes');
+    d3.select('body').append('p').text('File Type: ' + fileType);
 }
 
-function printFile(data) {
+var printFile = function(data) {
     // Get a list of keys
     var keys = [];
     for (var key in data[0]) {
         if (data[0].hasOwnProperty(key)) keys.push(key);
     }
     console.log(keys);
+
+    var columnNameString = '';
+    for(var i = 0; i < keys.length; i++) {
+        columnNameString = columnNameString + keys[i] + ' ';
+    }
+    d3.select('body').append('p').text(columnNameString);
 
     d3.select('body')
       .data(data)
@@ -51,3 +62,5 @@ function printFile(data) {
       })
 }
 
+
+// Main
